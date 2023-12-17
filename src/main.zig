97fc534457c543b74df729b23aa5c45f5617bc6b
pyvyx <pyvyx@web.de>
2023-12-17 22:24:17 +0100

    Added args parsing

--- src/main.zig
+++ src/main.zig
@@ -45,35 +45,144 @@ pub fn write_file_header(writer: anytype) !void
     _ = try writer.write("// more infos and bug-reports: https://github.com/pyvyx/FileAsCode              //\n");
     _ = try writer.write("//                                                                              //\n");
     _ = try writer.write("//////////////////////////////////////////////////////////////////////////////////\n\n");
-    _ = try writer.write("static const unsigned char sg_File_as_code =\n");
+    _ = try writer.write("static const unsigned char sg_File_as_code[] =\n");
+}
+
+
+const Settings = struct {
+    input_file: [] const u8 = undefined,
+    output_file: [] const u8 = undefined,
+    valid: bool = true,
+    c_style: bool = true,
+    help: bool = false,
+    inline_vars: bool = false,
+    uncompressed_data: bool = false
+};
+
+
+pub fn print(comptime format: []const u8, args: anytype) void
+{
+    std.io.getStdOut().writer().print(format, args) catch return;
+}
+
+
+pub fn print_help(path: [] const u8) void
+{
+    print("Usage: {s} [options]\nOptions:\n", .{path});
+    print("        -i   | --input  [FILE]   Input file path\n", .{});
+    print("        -o   | --output [FILE]   Output file path\n", .{});
+    print("        -c   | --c-style         Create a C-Style array\n", .{});
+    print("        -c++ | --cplusplus       Create a C++-Style array\n", .{});
+    print("        -h   | --help            Show this info message\n", .{});
+    print("        -u   | --uncompressed    Write uncompressed data to file\n", .{});
+    print("        -l   | --inline          Inline the variables (starting from C++17)\n", .{});
+}
+
+
+pub fn parse_args(args: [][] u8) Settings
+{
+    var settings: Settings = .{
+        .valid = args.len > 1
+    };
+
+    var skip: bool = true;
+    for (0.., args) |i, arg|
+    {
+        if (skip)
+        {
+            skip = false;
+            continue;
+        }
+
+        if ((std.mem.eql(u8, arg, "-i") or std.mem.eql(u8, arg, "--input")))
+        {
+            if (i + 1 != args.len)
+            {
+                skip = true;
+                settings.input_file = args[i+1];
+            }
+            else
+            {
+                std.io.getStdErr().writer().print("Missing file path after '{s}'\nTry '--help' for additional information\n", .{arg}) catch return settings;
+                return settings;
+            }
+        }
+        else if ((std.mem.eql(u8, arg, "-o") or std.mem.eql(u8, arg, "--output")))
+        {
+            if (i + 1 != args.len)
+            {
+                skip = true;
+            settings.output_file = args[i+1];
+            }
+            else
+            {
+                std.io.getStdErr().writer().print("Missing file path after '{s}'\nTry '--help' for additional information\n", .{arg}) catch return settings;
+                return settings;
+            }
+        }
+        else if (std.mem.eql(u8, arg, "-c") or std.mem.eql(u8, arg, "--c-style"))
+        {
+            settings.c_style = true;
+        }
+        else if (std.mem.eql(u8, arg, "-c++") or std.mem.eql(u8, arg, "--cplusplus"))
+        {
+            settings.c_style = false;
+        }
+        else if (std.mem.eql(u8, arg, "-h") or std.mem.eql(u8, arg, "--help"))
+        {
+            print_help(args[0]);
+            settings.help = true;
+            return settings;
+        }
+        else if (std.mem.eql(u8, arg, "-u") or std.mem.eql(u8, arg, "--uncompressed"))
+        {
+            settings.uncompressed_data = true;
+        }
+        else if (std.mem.eql(u8, arg, "-l") or std.mem.eql(u8, arg, "--inline"))
+        {
+            settings.inline_vars = true;
+        }
+        else
+        {
+            settings.help = true;
+            std.io.getStdErr().writer().print("Unknown option '{s}'\nTry '--help' for additional information\n", .{arg}) catch return settings;
+            return settings;
+        }
+    }
+
+    return settings;
 }
 
 
 pub fn main() !void
 {
     const allocator = std.heap.page_allocator;
-
     const args = try std.process.argsAlloc(allocator);
     defer std.process.argsFree(allocator, args);
+    const settings = parse_args(args);
 
-    if (args.len == 1)
+    if (settings.help)
     {
-        std.debug.print("Please provide a path", .{});
+        return;
+    }
+    else if (!settings.valid)
+    {
+        std.io.getStdErr().writer().print("Usage: {s} [options]\nTry '--help' for additional information\n", .{args[0]}) catch return;
         return;
     }
 
-    var file = try std.fs.cwd().openFile(args[0], .{});
-    defer file.close();
-
-    const result = try file.readToEndAlloc(allocator, (try file.stat()).size);
-    defer allocator.free(result);
-    _ = try file.readAll(result);
-
-    var write = try std.fs.cwd().createFile("result.txt", .{});
-    defer write.close();
-
-    var buf = std.io.bufferedWriter(write.writer());
-    try write_file_header(buf.writer());
-    try write_bytes_format(buf.writer(), result);
-    try buf.flush();
+    //var file = try std.fs.cwd().openFile(args[0], .{});
+    //defer file.close();
+//
+    //const result = try file.readToEndAlloc(allocator, (try file.stat()).size);
+    //defer allocator.free(result);
+    //_ = try file.readAll(result);
+//
+    //var write = try std.fs.cwd().createFile("result.txt", .{});
+    //defer write.close();
+//
+    //var buf = std.io.bufferedWriter(write.writer());
+    //try write_file_header(buf.writer());
+    //try write_bytes_format(buf.writer(), result);
+    //try buf.flush();
 }
